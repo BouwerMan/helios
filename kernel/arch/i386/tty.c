9a0f9800ea4a2a8ba9e03886fe8364c6b72f8e9b
Dylan Parks <dylan@bouwerbytes.com>
2024-01-02 21:01:13 -0600

    Reworked project to be similar to meaty skeleton tutorial
    Libc is basically non existant however build.sh works

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -1,55 +1,10 @@
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
-#if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
+#include <kernel/tty.h>
 
-/* This tutorial will only work for the 32-bit ix86 targets. */
-#if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
-
-/* Hardware text mode color constants. */
-enum vga_color
-{
-    VGA_COLOR_BLACK = 0,
-    VGA_COLOR_BLUE = 1,
-    VGA_COLOR_GREEN = 2,
-    VGA_COLOR_CYAN = 3,
-    VGA_COLOR_RED = 4,
-    VGA_COLOR_MAGENTA = 5,
-    VGA_COLOR_BROWN = 6,
-    VGA_COLOR_LIGHT_GREY = 7,
-    VGA_COLOR_DARK_GREY = 8,
-    VGA_COLOR_LIGHT_BLUE = 9,
-    VGA_COLOR_LIGHT_GREEN = 10,
-    VGA_COLOR_LIGHT_CYAN = 11,
-    VGA_COLOR_LIGHT_RED = 12,
-    VGA_COLOR_LIGHT_MAGENTA = 13,
-    VGA_COLOR_LIGHT_BROWN = 14,
-    VGA_COLOR_WHITE = 15,
-};
-
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
-{
-    return fg | bg << 4;
-}
-
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
-{
-    return (uint16_t)uc | (uint16_t)color << 8;
-}
-
-size_t strlen(const char* str)
-{
-    size_t len = 0;
-    while (str[len])
-        len++;
-    return len;
-}
+#include "vga.h"
 
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
@@ -68,7 +23,7 @@ static inline void outb(uint16_t port, uint8_t val)
      * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
 }
 
-void disable_cursor()
+void terminal_disable_cursor()
 {
     outb(0x3D4, 0x0A);
     outb(0x3D5, 0x20);
@@ -80,10 +35,8 @@ void terminal_initialize(void)
     terminal_column = 0;
     terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
     terminal_buffer = (uint16_t*)0xB8000;
-    for (size_t y = 0; y < VGA_HEIGHT; y++)
-    {
-        for (size_t x = 0; x < VGA_WIDTH; x++)
-        {
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
             const size_t index = y * VGA_WIDTH + x;
             terminal_buffer[index] = vga_entry(' ', terminal_color);
         }
@@ -103,8 +56,7 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c)
 {
-    switch (c)
-    {
+    switch (c) {
     case '\n':
         terminal_row++;
         terminal_column = 0;
@@ -114,25 +66,20 @@ void terminal_putchar(char c)
         terminal_column++;
     }
     // Handle overflowing on right side
-    if (terminal_column >= VGA_WIDTH)
-    {
+    if (terminal_column >= VGA_WIDTH) {
         terminal_column = 0;
         terminal_row++;
     }
 
     size_t i, j;
-    if (terminal_row >= VGA_HEIGHT)
-    {
-        for (i = 0; i < VGA_WIDTH; i++)
-        {
-            for (j = 0; j < VGA_HEIGHT; j++)
-            {
+    if (terminal_row >= VGA_HEIGHT) {
+        for (i = 0; i < VGA_WIDTH; i++) {
+            for (j = 0; j < VGA_HEIGHT; j++) {
                 terminal_buffer[j * VGA_WIDTH + i] = terminal_buffer[(j + 1) * VGA_WIDTH + i];
             }
         }
         // Also clear out the bottom row
-        for (i = 0; i < VGA_WIDTH; i++)
-        {
+        for (i = 0; i < VGA_WIDTH; i++) {
             terminal_putentryat(' ', terminal_color, i, VGA_HEIGHT - 1);
         }
 
@@ -149,22 +96,4 @@ void terminal_write(const char* data, size_t size)
 void terminal_writestring(const char* data)
 {
     terminal_write(data, strlen(data));
-}
-
-void kernel_main(void)
-{
-    /* Initialize terminal interface */
-    terminal_initialize();
-
-    /* Newline support is left as an exercise. */
-    terminal_writestring("Hello, kernel World!\nNewline Test\n");
-    for (size_t i = 0; i < VGA_HEIGHT - 2; i++)
-    {
-        char test[3];
-        test[0] = i + '0';
-        test[1] = '\n';
-        test[2] = '\0';
-        terminal_writestring(test);
-    }
-    disable_cursor();
 }
