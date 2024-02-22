433e53825de73d3d9871bb877b875c9e535b1042
Dylan Parks <dylan@bouwerbytes.com>
2024-02-22 11:22:09 +0100

    Changed buildsystem to make

--- HeliOS/kernel/kernel.c
+++ HeliOS/kernel/kernel.c
@@ -13,100 +13,100 @@
 
 void kernel_early(multiboot_info_t* mbd, uint32_t magic)
 {
-        /* Initialize terminal interface */
-        tty_initialize();
+    /* Initialize terminal interface */
+    tty_initialize();
 
-        /* Make sure the magic number matches for memory mapping*/
-        if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
-                panic("invalid magic number!");
+    /* Make sure the magic number matches for memory mapping*/
+    if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        panic("invalid magic number!");
+    }
+
+    /* Check bit 6 to see if we have a valid memory map */
+    if (!(mbd->flags >> 6 & 0x1)) {
+        panic("invalid memory map given by GRUB bootloader");
+    }
+    /* Loop through the memory map and display the values */
+    int i;
+    uint32_t length = 0;
+    for (i = 0; i < mbd->mmap_length;
+         i += sizeof(multiboot_memory_map_t)) {
+        multiboot_memory_map_t* mmmt = (multiboot_memory_map_t*)(mbd->mmap_addr + i);
+
+        printf("Start Addr: %x | Length: %x | Size: %x | Type: %d\n",
+            mmmt->addr_low, mmmt->len_low, mmmt->size, mmmt->type);
+
+        length += mmmt->len_low;
+        if (mmmt->type == MULTIBOOT_MEMORY_AVAILABLE) {
+            /*
+             * Do something with this memory block!
+             * BE WARNED that some of memory shown as availiable is actually
+             * actively being used by the kernel! You'll need to take that
+             * into account before writing to memory!
+             */
         }
+    }
+    printf("Memory Length: %dMB\n", length / 1024 / 1024);
 
-        /* Check bit 6 to see if we have a valid memory map */
-        if (!(mbd->flags >> 6 & 0x1)) {
-                panic("invalid memory map given by GRUB bootloader");
-        }
-        /* Loop through the memory map and display the values */
-        int i;
-        uint32_t length = 0;
-        for (i = 0; i < mbd->mmap_length;
-             i += sizeof(multiboot_memory_map_t)) {
-                multiboot_memory_map_t* mmmt = (multiboot_memory_map_t*)(mbd->mmap_addr + i);
+    tty_enable_cursor(0, 0);
+    // tty_disable_cursor();
 
-                printf("Start Addr: %x | Length: %x | Size: %x | Type: %d\n",
-                    mmmt->addr_low, mmmt->len_low, mmmt->size, mmmt->type);
+    puts("Initializing GDT");
+    gdt_init();
 
-                length += mmmt->len_low;
-                if (mmmt->type == MULTIBOOT_MEMORY_AVAILABLE) {
-                        /*
-                         * Do something with this memory block!
-                         * BE WARNED that some of memory shown as availiable is actually
-                         * actively being used by the kernel! You'll need to take that
-                         * into account before writing to memory!
-                         */
-                }
-        }
-        printf("Memory Length: %dMB\n", length / 1024 / 1024);
+    puts("Initializing IDT");
+    idt_init();
 
-        tty_enable_cursor(0, 0);
-        // tty_disable_cursor();
+    puts("Initializing ISRs");
+    isr_init();
 
-        puts("Initializing GDT");
-        gdt_init();
+    puts("Initializing IRQs");
+    irq_init();
 
-        puts("Initializing IDT");
-        idt_init();
+    puts("Initializing Timer");
+    timer_init();
 
-        puts("Initializing ISRs");
-        isr_init();
+    puts("Initializing Keyboard");
+    keyboard_init();
 
-        puts("Initializing IRQs");
-        irq_init();
+    puts("Initalizing Paging");
+    mmap_init(mbd);
 
-        puts("Initializing Timer");
-        timer_init();
-
-        puts("Initializing Keyboard");
-        keyboard_init();
-
-        puts("Initalizing Paging");
-        mmap_init(mbd);
-
-        printf("Memory: %d\n", mbd->mem_upper - mbd->mem_lower);
+    printf("Memory: %d\n", mbd->mem_upper - mbd->mem_lower);
 }
 
 void kernel_main()
 {
-        printf("Welcome to %s. Version: %s\n", KERNEL_NAME, KERNEL_VERSION);
-        printf("Detected CPU: ");
-        cpu_print_model();
+    printf("Welcome to %s. Version: %s\n", KERNEL_NAME, KERNEL_VERSION);
+    printf("Detected CPU: ");
+    cpu_print_model();
 
-        // Testing that interrupts are active by waiting for the timer to tick
-        puts("Testing Interrupts");
-        timer_poll();
-        tty_setcolor(VGA_COLOR_GREEN);
-        puts("Interrupts passed");
-        tty_setcolor(VGA_COLOR_LIGHT_GREY);
+    // Testing that interrupts are active by waiting for the timer to tick
+    puts("Testing Interrupts");
+    timer_poll();
+    tty_setcolor(VGA_COLOR_GREEN);
+    puts("Interrupts passed");
+    tty_setcolor(VGA_COLOR_LIGHT_GREY);
 
 #if 1
-        page_directory_clear();
-        init_paging();
+    page_directory_clear();
+    init_paging();
 #endif
 
 #if 0
-        // Testing paging
-        uint32_t new_frame = allocate_frame();
-        uint32_t new_frame_addr = mmap_read(new_frame, MMAP_GET_ADDR);
-        printf("New frame allocated at: 0x%x\n", new_frame_addr);
-        uint32_t new_frame_two = allocate_frame();
-        uint32_t new_frame_addr_two = mmap_read(new_frame_two, MMAP_GET_ADDR);
-        printf("New frame allocated at: 0x%x\n", new_frame_addr_two);
-        free_frame(new_frame);
-        uint32_t new_frame_three = allocate_frame();
-        uint32_t new_frame_addr_three = mmap_read(new_frame, MMAP_GET_ADDR);
-        printf("New frame allocated at: 0x%x\n", new_frame_addr);
+    // Testing paging
+    uint32_t new_frame = allocate_frame();
+    uint32_t new_frame_addr = mmap_read(new_frame, MMAP_GET_ADDR);
+    printf("New frame allocated at: 0x%x\n", new_frame_addr);
+    uint32_t new_frame_two = allocate_frame();
+    uint32_t new_frame_addr_two = mmap_read(new_frame_two, MMAP_GET_ADDR);
+    printf("New frame allocated at: 0x%x\n", new_frame_addr_two);
+    free_frame(new_frame);
+    uint32_t new_frame_three = allocate_frame();
+    uint32_t new_frame_addr_three = mmap_read(new_frame, MMAP_GET_ADDR);
+    printf("New frame allocated at: 0x%x\n", new_frame_addr);
 #endif
 
-        // Stopping us from exiting kernel
-        for (;;)
-                ;
+    // Stopping us from exiting kernel
+    for (;;)
+        ;
 }
--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -11,120 +11,120 @@ char buffer[BUF_SIZE];
 // TODO: MAKE THIS BETTER I AM LITERALLY BANDAIDING THE ENTIRE THING
 static char* parse_num(unsigned int value, unsigned int base)
 {
-        size_t i = 0;
-        memset(buffer, 0, BUF_SIZE);
+    size_t i = 0;
+    memset(buffer, 0, BUF_SIZE);
 
-        // Gets digit from right to left and converts to ascii
-        while (value != 0) {
-                buffer[i] = (value % 10) + '0';
-                i++;
-                value /= 10;
-        };
-        // Iterates over array and prints it
-        for (i; i > 0; i--) {
-                putchar(buffer[i - 1]); // TODO: WTF is this
-        }
-        return buffer;
+    // Gets digit from right to left and converts to ascii
+    while (value != 0) {
+        buffer[i] = (value % 10) + '0';
+        i++;
+        value /= 10;
+    };
+    // Iterates over array and prints it
+    for (i; i > 0; i--) {
+        putchar(buffer[i - 1]); // TODO: WTF is this
+    }
+    return buffer;
 }
 
 static char* parse_hex(unsigned int value)
 {
-        int i = 8;
-        size_t index = 0;
-        memset(buffer, 0, BUF_SIZE);
+    int i = 8;
+    size_t index = 0;
+    memset(buffer, 0, BUF_SIZE);
 
-        while (i-- > 0)
-                buffer[index++] = "0123456789abcdef"[(value >> (i * 4)) & 0xF];
-        return buffer;
+    while (i-- > 0)
+        buffer[index++] = "0123456789abcdef"[(value >> (i * 4)) & 0xF];
+    return buffer;
 }
 
 static bool print(const char* data, size_t length)
 {
-        const unsigned char* bytes = (const unsigned char*)data;
-        for (size_t i = 0; i < length; i++)
-                if (putchar(bytes[i]) == EOF)
-                        return false;
-        return true;
+    const unsigned char* bytes = (const unsigned char*)data;
+    for (size_t i = 0; i < length; i++)
+        if (putchar(bytes[i]) == EOF)
+            return false;
+    return true;
 }
 
 int printf(const char* restrict format, ...)
 {
-        va_list parameters;
-        va_start(parameters, format);
+    va_list parameters;
+    va_start(parameters, format);
 
-        int written = 0;
+    int written = 0;
 
-        while (*format != '\0') {
-                size_t maxrem = INT_MAX - written;
+    while (*format != '\0') {
+        size_t maxrem = INT_MAX - written;
 
-                if (format[0] != '%' || format[1] == '%') {
-                        if (format[0] == '%')
-                                format++;
-                        size_t amount = 1;
-                        while (format[amount] && format[amount] != '%')
-                                amount++;
-                        if (maxrem < amount) {
-                                // TODO: Set errno to EOVERFLOW.
-                                return -1;
-                        }
-                        if (!print(format, amount))
-                                return -1;
-                        format += amount;
-                        written += amount;
-                        continue;
-                }
-
-                const char* format_begun_at = format++;
-
-                if (*format == 'c') {
-                        format++;
-                        char c = (char)va_arg(parameters, int /* char promotes to int */);
-                        if (!maxrem) {
-                                // TODO: Set errno to EOVERFLOW.
-                                return -1;
-                        }
-                        if (!print(&c, sizeof(c)))
-                                return -1;
-                        written++;
-                } else if (*format == 's') {
-                        format++;
-                        const char* str = va_arg(parameters, const char*);
-                        size_t len = strlen(str);
-                        if (maxrem < len) {
-                                // TODO: Set errno to EOVERFLOW.
-                                return -1;
-                        }
-                        if (!print(str, len))
-                                return -1;
-                        written += len;
-                } else if (*format == 'x') { // I've stolen this which is why it is kinda a mess/different style.
-                        format++;
-                        unsigned int value = (unsigned int)va_arg(parameters, int);
-                        parse_hex(value);
-                        size_t len = strlen(buffer);
-                        print(buffer, len);
-                        written += len;
-                } else if (*format == 'd') {
-                        format++;
-                        unsigned int value = (unsigned int)va_arg(parameters, int);
-                        parse_num(value, 10);
-                        size_t len = strlen(buffer);
-                        // print(buffer, len);
-                        written += len;
-                } else {
-                        format = format_begun_at;
-                        size_t len = strlen(format);
-                        if (maxrem < len) {
-                                // TODO: Set errno to EOVERFLOW.
-                                return -1;
-                        }
-                        if (!print(format, len))
-                                return -1;
-                        written += len;
-                        format += len;
-                }
+        if (format[0] != '%' || format[1] == '%') {
+            if (format[0] == '%')
+                format++;
+            size_t amount = 1;
+            while (format[amount] && format[amount] != '%')
+                amount++;
+            if (maxrem < amount) {
+                // TODO: Set errno to EOVERFLOW.
+                return -1;
+            }
+            if (!print(format, amount))
+                return -1;
+            format += amount;
+            written += amount;
+            continue;
         }
 
-        va_end(parameters);
-        return written;
+        const char* format_begun_at = format++;
+
+        if (*format == 'c') {
+            format++;
+            char c = (char)va_arg(parameters, int /* char promotes to int */);
+            if (!maxrem) {
+                // TODO: Set errno to EOVERFLOW.
+                return -1;
+            }
+            if (!print(&c, sizeof(c)))
+                return -1;
+            written++;
+        } else if (*format == 's') {
+            format++;
+            const char* str = va_arg(parameters, const char*);
+            size_t len = strlen(str);
+            if (maxrem < len) {
+                // TODO: Set errno to EOVERFLOW.
+                return -1;
+            }
+            if (!print(str, len))
+                return -1;
+            written += len;
+        } else if (*format == 'x') { // I've stolen this which is why it is kinda a mess/different style.
+            format++;
+            unsigned int value = (unsigned int)va_arg(parameters, int);
+            parse_hex(value);
+            size_t len = strlen(buffer);
+            print(buffer, len);
+            written += len;
+        } else if (*format == 'd') {
+            format++;
+            unsigned int value = (unsigned int)va_arg(parameters, int);
+            parse_num(value, 10);
+            size_t len = strlen(buffer);
+            // print(buffer, len);
+            written += len;
+        } else {
+            format = format_begun_at;
+            size_t len = strlen(format);
+            if (maxrem < len) {
+                // TODO: Set errno to EOVERFLOW.
+                return -1;
+            }
+            if (!print(format, len))
+                return -1;
+            written += len;
+            format += len;
+        }
+    }
+
+    va_end(parameters);
+    return written;
 }
