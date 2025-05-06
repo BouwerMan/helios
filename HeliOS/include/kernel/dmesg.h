857f8d1ec07f8ed54e2c8acbea6d0c7689f31699
Dylan Parks <dylan@bouwerbytes.com>
2025-05-06 18:05:30 -0500

    removed extra comments

--- HeliOS/include/kernel/dmesg.h
+++ HeliOS/include/kernel/dmesg.h
@@ -7,10 +7,3 @@ void dmesg_enqueue(const char* str, size_t len);
 void dmesg_wait();
 void dmesg_wake();
 void dmesg_task_entry(void);
-
-// TODO: ChatGPT made these prototypes, fill in code and rename.
-
-// void log_task_entry(void);
-// These 2 just use flag to know if it needs to log
-// void wake_log_task();
-// void sleep_until_log_not_empty();
